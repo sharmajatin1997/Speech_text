81ef96c1860613f73c946a5d87f787ffa51575d4
Jatin Sharma <80152469+sharmajatin1997@users.noreply.github.com>
2023-05-29 18:10:17 +0530

    Initial commit

---
+++ lib/homepage.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final SpeechToText _speechToText = SpeechToText();
+  bool speechEnabled = false;
+  bool _speech = false;
+  String _lastWords = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _initSpeech();
+  }
+
+  void _initSpeech() async {
+    speechEnabled = await _speechToText.initialize();
+    setState(() {});
+  }
+
+  void _startListening() async {
+    await _speechToText.listen(onResult: _onSpeechResult);
+    setState(() {});
+  }
+
+  void _stopListening() async {
+    await _speechToText.stop();
+    setState(() {});
+  }
+
+  void _onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      _lastWords = result.recognizedWords;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Speech Demo'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: const Text(
+                'Recognized words:',
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                child: Text(
+                  _speech
+                      ? _lastWords
+                      // : _speechEnabled
+                       :'Tap the microphone to start listening...'
+                      // ? 'Tap the microphone to start listening...'
+                      // : 'Speech not available',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 60,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: (){
+                      _speech=true;
+                      _startListening();
+                    },
+                      child: const Icon(Icons.mic_off,size: 40)),
+                  const SizedBox(width: 30),
+                  GestureDetector(
+                      onTap: (){
+                        _speech=false;
+                        _stopListening();
+                      },
+                      child: const Icon(Icons.mic,size: 40)),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+
+    );
+  }
+}
